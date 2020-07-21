8307705da49cfb644a36ffcaa4a2da01c70eda8b
KrinzXP <krinkovabyss@gmail.com>
2020-07-21 09:46:02 +0700

    Add files via upload

--- src/app/video-grid-component/video-grid-component.component.ts
+++ src/app/video-grid-component/video-grid-component.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {VideosServiceService} from "../videos-service.service";
+import {any} from "codelyzer/util/function";
 
 
 
@@ -10,17 +12,53 @@ import { Component, OnInit } from '@angular/core';
 export class VideoGridComponentComponent implements OnInit {
 
   videoTile = [];
-  constructor() { }
+  video = [];
+  channelIds = ['UCWFKCr40YwOZQx8FHU_ZqqQ', 'UCRcgy6GzDeccI7dkbbBna3Q', 'UC5QtJAXO631Ry0WUQuMzfHg', 'UCoY57POHD3aHk6qTsA65gdQ', 'UCY7gYHez6sxlK9TRSANHTaw'];
+  constructor(private videoService: VideosServiceService) { }
 
   ngOnInit(): void {
-    this.videoTile = [
-      {user: 'John Appleseed',text: 'One', cols: 2, rows: 2, color: 'lightblue', viewers: 3939},
-      {user: 'Mira Akbar',text: 'Two', cols: 1, rows: 1, color: 'lightgreen', viewers: 420},
-      {user: 'Sarah Toh',text: 'Three', cols: 1, rows: 1, color: 'lightpink', viewers: 1337},
-      {user: 'Aji Nomoto',text: 'Four', cols: 1, rows: 1, color: '#DDBDF1', viewers: 765},
-      {user: 'Homura Tabata',text: 'Five', cols: 1, rows: 1, color: '#DDBDF1', viewers: 243},
-      {user: '',text: 'Share your video', cols: 1, rows: 1, color: '#FFFFFF'}
-    ];
+
+    //sample API call
+    //TODO: supposedly able to search for videos. but api key exceeded the daily quota.
+    for (var index = 0; index < this.channelIds.length; index++) {
+      this.videoService.getVideosForChanel(this.channelIds[index], 1).subscribe(list => {
+        for (let element of list["items"]) {
+          this.video.push(element);
+        }
+      })
+    }
+
+    console.log(this.video);
+
+    //TODO: For some reason, unable to display video tiles........
+    var length = this.video.length;
+
+    for (var index = 0; index < length; index++) {
+      var cols = 0;
+      var rows = 0;
+
+      if(index == 0) {
+        cols = 2;
+        rows = 2;
+      } else {
+        cols = 1;
+        rows = 2;
+      }
+      let videoJson = this.video[index];
+      let json = {
+        user: videoJson.snippet.channelTitle,
+        text: videoJson.snippet.title,
+        cols: cols,
+        rows: rows,
+        viewers: 0,
+        videoId: videoJson.id.videoId,
+        color: 'lightblue'
+      };
+      console.log(json);
+      this.videoTile.push(json);
+    }
+
+    console.log(this.videoTile);
   }
 
 }
